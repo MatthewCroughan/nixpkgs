be94e53ae2cf8286e1e7ee199e629ad5a7e44d82
matthewcroughan <matt@croughan.sh>
2023-04-03 18:10:23 +0100

    wip

--- pkgs/applications/misc/pgmodeler/default.nix
+++ pkgs/applications/misc/pgmodeler/default.nix
@@ -7,6 +7,8 @@
 , qtwayland
 , qtsvg
 , postgresql
+, copyDesktopItems
+, makeDesktopItem
 }:
 
 stdenv.mkDerivation rec {
@@ -20,11 +22,29 @@ stdenv.mkDerivation rec {
     sha256 = "sha256-yvVgBfJLjEynsqxQisDfOM99C8/QM0F44RIHAmxh4uU=";
   };
 
-  nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook ];
+  nativeBuildInputs = [ pkg-config qmake wrapQtAppsHook copyDesktopItems ];
   qmakeFlags = [ "pgmodeler.pro" "CONFIG+=release" ];
 
   # todo: libpq would suffice here. Unfortunately this won't work, if one uses only postgresql.lib here.
   buildInputs = [ postgresql qtsvg qtwayland ];
+
+  desktopItems = [(makeDesktopItem {
+    desktopName = "pgModeler";
+    name = "pgModeler";
+    exec = pname;
+    icon = "pgmodeler";
+    comment = meta.description;
+    type = "Application";
+    categories = [ "Misc" ];
+  })];
+
+  fixupPhase = ''
+    runHook preFixup
+
+    ln -s $out/share/pgmodeler/conf/pgmodeler_logo.png $out/share/icons/pgmodeler.png
+
+    runHook postFixup
+  '';
 
   meta = with lib; {
     description = "A database modeling tool for PostgreSQL";
